452f84ebd2158825893b3ea3f218aa344c6c785d
Jesse Bordoe <jesse.bordoe@gmail.com>
2025-07-14 20:11:28 +0200

    refactor: move logic out of cli entrypoint script

--- src/audio_collage/cli.py
+++ src/audio_collage/cli.py
@@ -2,75 +2,21 @@
 
 import sys
 import typer
-from strenum import StrEnum
 from rich import print
-from rich.progress import Progress, SpinnerColumn, TextColumn, track
+from rich.progress import track
 from typing import Callable
 
 import numpy as np
 
 from .util import Util
-from .collager import Collager
+from .audio_mapper import AudioMapper
 from .audio_file import AudioFile
+from .collager import Collager
 
-DeclickFn = StrEnum('Declickfn', {k: k for k in ['sigmoid', 'linear']})
-DistanceFn = StrEnum('DistanceFn', {k: k for k in ['mfcc', 'fast_mfcc', 'mean_mfcc']})
+DeclickFn = Collager.DeclickFn
+DistanceFn = Collager.DistanceFn
 
 app = typer.Typer()
-
-def create_collage(
-    target_file: str,
-    sample_file: str,
-    outpath: str,
-    declick_fn: DeclickFn,
-    declick_ms: int,
-    distance_fn: DistanceFn
-):
-    """
-    This is the core logic for creating a collage.
-    """
-    default_dc_ms = {
-        'sigmoid': 20,
-        'linear': 70,
-    }
-    if declick_fn:
-        declick_ms = declick_ms or default_dc_ms[declick_fn]
-    else:
-        declick_ms = 0
-
-    sample_audio = Util.read_audio(sample_file)
-    target_audio = Util.read_audio(target_file)
-
-    windows = [500, 200, 100, 50]
-    windows = [i + declick_ms for i in windows]
-
-    dist_fn_map: Dict[str, Callable[[AudioFile, AudioFile], float]] = {
-        'mfcc': Util.mfcc_dist,
-        'fast_mfcc': Util.fast_mfcc_dist,
-        'mean_mfcc': Util.mean_mfcc_dist,
-    }
-    
-    selected_distance_fn = dist_fn_map.get(distance_fn.value)
-    if not selected_distance_fn:
-        print(f'[yellow]Invalid distance function [yellow bold]{distance_fn}[yellow]!')
-        raise typer.Exit(code=1)
-
-    collager = Collager(sample_audio, target_audio, distance_fn=selected_distance_fn)
-    selected_snippets = collager.collage(
-        windows=windows,
-        overlap_ms=declick_ms,
-    )
-
-    output_audio = Util.concatenate_audio(
-        track(selected_snippets, description="[cyan]Concatenating samples..."),
-        declick_fn=declick_fn,
-        declick_ms=declick_ms
-    )
-
-    print(f'[cyan]Saving collage to [yellow]{outpath}[cyan]...')
-    Util.save_audio(output_audio, outpath)
-
-    print('[green bold]Done!')
 
 
 @app.command()
@@ -96,7 +42,7 @@ def collage(
     Create a collage based on a given audio file using snippets from another.
     This is a thin wrapper around the create_collage function.
     """
-    create_collage(
+    Collager.create_collage(
         target_file=target_file,
         sample_file=sample_file,
         outpath=outpath,
@@ -127,7 +73,7 @@ def example():
     """
     Create an example collage using Amen Brother and Zimba Ku breakbeats.
     """
-    create_collage(
+    Collager.create_collage(
         target_file='./docs/audio/breaks/amen_brother.wav',
         sample_file='./docs/audio/breaks/black_heat__zimba_ku.wav',
         outpath='./collage.wav',
--- src/audio_collage/collager.py
+++ src/audio_collage/collager.py
@@ -1,129 +1,66 @@
-from rich.progress import Progress, track
-from typing import Dict, List, Tuple
-
-import os
-import pickle
-from vptree import VPTree
-
-from .audio_file import AudioFile
 from .util import Util
+from .audio_mapper import AudioMapper
+from .audio_file import AudioFile
 
-CACHE_DIR = '.cache'
+from rich import print
+from rich.progress import Progress, SpinnerColumn, TextColumn, track
+from strenum import StrEnum
 
 class Collager:
-    def __init__(
-        self,
-        target_audio: AudioFile,
-        sample_audio: AudioFile,
-        distance_fn: callable = Util.mean_mfcc_dist
+    DeclickFn = StrEnum('Declickfn', {k: k for k in ['sigmoid', 'linear']})
+    DistanceFn = StrEnum('DistanceFn', {k: k for k in ['mfcc', 'fast_mfcc', 'mean_mfcc']})
+
+    @staticmethod
+    def create_collage(
+        target_file: str,
+        sample_file: str,
+        outpath: str,
+        declick_fn: DeclickFn,
+        declick_ms: int,
+        distance_fn: DistanceFn
     ):
-        self.source: AudioFile = sample_audio
-        self.target: AudioFile = target_audio
-        self.indices: Dict[int, VPTree] = {}
-        self.distance_fn = distance_fn
+        """
+        This is the core logic for creating a collage.
+        """
+        default_dc_ms = {
+            'sigmoid': 20,
+            'linear': 70,
+        }
+        if declick_fn:
+            declick_ms = declick_ms or default_dc_ms[declick_fn]
+        else:
+            declick_ms = 0
 
-    def collage(self, windows: List[int] = [200], overlap_ms: int = 0) -> List[AudioFile]:
-        self._chop(windows)
-        selected_snippets: List[AudioFile] = []
+        sample_audio = Util.read_audio(sample_file)
+        target_audio = Util.read_audio(target_file)
 
-        Util.extract_features(self.target)
-        target_sr = self.target.sample_rate
+        windows = [500, 200, 100, 50]
+        windows = [i + declick_ms for i in windows]
 
-        pointer: int = 0
-        with Progress() as progress:
-            task = progress.add_task('[cyan]Selecting samples...', total=100)
-            while pointer < self.target.timeseries.size:
-                pct_complete = (pointer / self.target.timeseries.size) * 100 if pointer else 0
-                progress.update(task, completed=pct_complete)
+        dist_fn_map: Dict[str, Callable[[AudioFile, AudioFile], float]] = {
+            'mfcc': Util.mfcc_dist,
+            'fast_mfcc': Util.fast_mfcc_dist,
+            'mean_mfcc': Util.mean_mfcc_dist,
+        }
 
-                best_snippet: AudioFile = None
-                best_snippet_dist: float = float('inf')
-                best_snippet_window: int = 0
-                for window in windows:
-                    window_size_frames = int((window / 1000) * target_sr)
-                    target_chunk = AudioFile(
-                            self.target.timeseries[pointer:pointer + window_size_frames - 1],
-                            target_sr,
-                            )
-                    Util.extract_features(target_chunk)
+        selected_distance_fn = dist_fn_map.get(distance_fn.value)
+        if not selected_distance_fn:
+            print(f'[yellow]Invalid distance function [yellow bold]{distance_fn}[yellow]!')
+            raise typer.Exit(code=1)
 
-                    nearest_dist, nearest = self._search(target_chunk, window)
-                    if nearest_dist < best_snippet_dist:
-                        best_snippet_dist = nearest_dist
-                        best_snippet = nearest
-                        best_snippet_window = window_size_frames
-
-                selected_snippets.append(best_snippet)
-                pointer += best_snippet_window - int((overlap_ms /1000) * target_sr)
-            progress.update(task, completed=100)
-
-        return selected_snippets
-
-    def _chop(self, windows: List[int]) -> None:
-        with Progress() as progress:
-            task = progress.add_task(
-                description="[cyan]Chopping and analysing sample audio...",
-                total=self.source.n_samples() * len(windows)
-            )
-            hash: str = self.source.hash()
-            for window in windows:
-                if self._cache_load(hash, window):
-                    progress.update(task, advance = self.source.n_samples())
-                    continue
-
-                sample_group: List[AudioFile] = Util.chop_audio(self.source, window)
-
-                for s in sample_group:
-                    Util.extract_features(s)
-                    progress.update(task, advance=int((window / 1000) * self.source.sample_rate))
-
-                self._index(sample_group, window, hash)
-
-    def _search(self, query_audio: AudioFile, key: int) -> Tuple[float, AudioFile]:
-        index = self.indices[key]
-        nearest_dist, nearest = index.get_nearest_neighbor(query_audio)
-        return nearest_dist, nearest
-
-    def _index(self, samples: List[AudioFile], key: int, hash: str) -> None:
-        tree = VPTree(samples, self.distance_fn)
-        self.indices[key] = tree
-        self._cache_vptree(hash, key)
-    
-    def _cache_path(self, hash: str, key: int) -> str:
-        return os.path.join(
-            CACHE_DIR,
-            f"{hash}.{key}.{self.distance_fn.__name__}.vptree"
+        mapper = AudioMapper(sample_audio, target_audio, distance_fn=selected_distance_fn)
+        selected_snippets = mapper.map_audio(
+            windows=windows,
+            overlap_ms=declick_ms,
         )
 
-    def _cache_vptree(self, hash: str, key: int) -> None:
-        if not os.path.exists(CACHE_DIR):
-            os.makedirs(CACHE_DIR)
+        output_audio = Util.concatenate_audio(
+            track(selected_snippets, description="[cyan]Concatenating samples..."),
+            declick_fn=declick_fn,
+            declick_ms=declick_ms
+        )
 
-        cache_path: str = self._cache_path(hash, key)
-        with open(cache_path, 'wb') as f:
-            pickle.dump(self.indices[key], f)
+        print(f'[cyan]Saving collage to [yellow]{outpath}[cyan]...')
+        Util.save_audio(output_audio, outpath)
 
-    def _clear_cache(self, hash: str, key: int) -> None:
-        cache_path: str = self._cache_path(hash, key)
-        if os.path.exists(cache_path):
-            os.remove(cache_path)
-
-    def _cache_load(self, hash: str, key: int) -> bool:
-        if not os.path.exists(CACHE_DIR):
-            return False
-
-        cache_path: str = self._cache_path(hash, key)
-        try:
-            with open(cache_path, 'rb') as f:
-                index: VPTree = pickle.load(f)
-                if index is None or not isinstance(index, VPTree):
-                    raise EOFError
-            
-                self.indices[key] = index
-                return True
-        except FileNotFoundError:
-            return False
-        except EOFError:
-            print(f'Error loading cache, removing file')
-            self._clear_cache(hash, key)
-            return False
+        print('[green bold]Done!')
