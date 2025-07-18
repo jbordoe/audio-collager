175dd4fc1e01d2dd1512c62e6ac05d6c38de6347
Jesse Bordoe <jesse.bordoe@gmail.com>
2025-07-18 13:38:09 +0200

    add unit tests for audio mapper callback logic

--- tests/test_audio_mapper.py
+++ tests/test_audio_mapper.py
@@ -2,6 +2,7 @@ from audio_collage.audio_dist import AudioDist
 from audio_collage.audio_mapper import AudioMapper
 from audio_collage.audio_segment import AudioSegment
 from audio_collage.collager import CollagerConfig
+from audio_collage.collage_progress_state import CollageProgressState
 from audio_collage.search.index_collection import SearchIndexCollection
 from audio_collage.util import Util
 
@@ -49,3 +50,49 @@ def test_map_audio(mocker):
             step_ms=100,
             step_factor=None
         )
+
+def test_map_audio_with_callback(mocker):
+    """
+    Test that the audio is mapped correctly with a progress callback.
+    """
+    mocker.patch.object(SearchIndexCollection, 'find_best_match', return_value=(111, 22, 3))
+    callback = mocker.Mock()
+    
+    config = CollagerConfig(
+        step_ms=100,
+        windows=[100, 200],
+        progress_callback=callback
+    )
+    source = AudioSegment(timeseries=np.arange(0, 10), sample_rate=100)
+    target = AudioSegment(timeseries=np.arange(0, 10), sample_rate=100)
+    mapper = AudioMapper(
+        source,
+        target,
+        config=config
+    )
+
+    mapper.map_audio()
+
+    callback.assert_any_call(CollageProgressState(
+        task=CollageProgressState.Task.CHOPPING,
+        starting=True,
+        current_step=0,
+        total_steps=2,
+        message="Chopping 2 windows"
+    ))
+    callback.assert_any_call(CollageProgressState(
+        task=CollageProgressState.Task.CHOPPING,
+        current_step=1,
+    ))
+    callback.assert_any_call(CollageProgressState(
+        task=CollageProgressState.Task.CHOPPING,
+        completed=True,
+        current_step=2,
+    ))
+    callback.assert_any_call(CollageProgressState(
+        task=CollageProgressState.Task.SELECTING,
+        starting=True,
+        current_step=0,
+        total_steps=100,
+        message="Selecting samples"
+    ))
